3664c22c01aa0881a8e1222311cc3a0201d5ffdf
nickfeltham33 <nick.feltham33@gmail.com>
2024-08-19 00:28:38 +0100

    comit

--- app/components/work/display/WorkDisplayWrap.tsx
+++ app/components/work/display/WorkDisplayWrap.tsx
@@ -19,9 +19,9 @@ const WorkWrap = ({ workData }) => {
           <h1 className="text-2xl">My Work</h1>
           <div className="w-8 h-[0.2rem] bg-gradient-green my-4" />
           <p className="text-2xs text-white-dark font-medium my-8 max-w-[26rem] tablet:text-xs">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sint
-            facilis soluta quia nemo explicabo in aut ab fuga. Vero beatae neque
-            sit saepe alias veritatis, repellendus ipsa corporis optio.
+            Here’s a selection of my work, ranging from practical projects to
+            creative experiments. Each one reflects my approach to design and
+            development, focused on solving problems and exploring new ideas.
           </p>
           <WorkDisplay workData={workData} />
         </div>
