670e5ba8640b89b591ff9c5801542174423c79a7
nickfeltham33 <nick.feltham33@gmail.com>
2024-09-14 19:53:23 +0100

    work to projects

--- app/_components/projects/display/ProjectsDisplayWrap.tsx
+++ app/_components/projects/display/ProjectsDisplayWrap.tsx
@@ -15,7 +15,7 @@ const ProjectsWrap = ({ projectsData }) => {
         className="relative items-center w-full min-h-screen pt-[20vh] pb-32"
       >
         <div className="w-4/5 mx-auto max-w-custom">
-          <h1 className="text-2xl">My Work</h1>
+          <h1 className="text-2xl">My Projects</h1>
           <div className="w-8 h-[0.2rem] bg-gradient-green my-4" />
           <p className="text-2xs text-white-dark font-medium my-8 max-w-[26rem] tablet:text-xs">
             Here’s a selection of my work, ranging from practical projects to
